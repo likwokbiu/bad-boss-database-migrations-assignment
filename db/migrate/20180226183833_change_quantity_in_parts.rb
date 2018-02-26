272b70ed5b41763ccb9e012189525ddf7f3670e2
likwokbiu <li_bill@ymail.com>
2018-02-26 13:48:37 -0500

    Change Quantity Type in Parts

---
+++ db/migrate/20180226183833_change_quantity_in_parts.rb
@@ -0,0 +1,14 @@
+class ChangeQuantityInParts < ActiveRecord::Migration[5.0]
+  def up
+    change_table :parts do |t|
+      t.change :quantity, :decimal
+    end
+  end
+
+  def down
+    change_table :parts do |t|
+      t.change :quantity, :integer
+    end
+  end
+
+end
